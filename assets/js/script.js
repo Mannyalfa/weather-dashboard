6c48c452d7d0f53ee6d18092c090c319f0d1e2d7
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-06-02 23:50:34 -0400

    html mostly complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,79 @@
 $(document).ready(function () {
-    //Display current time in 12 hour format
+    //Display current time in 12 hour format-temporary until weather script complete
 	setInterval(function () {
 		$("#currentDay").text(moment().format("dddd MMMM Do, YYYY h:mm:ss A"));
 	}, 1000);
+   //ApI key for weather data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//this closes the entire script: DON'T TOUCH IT!!!	
 });
