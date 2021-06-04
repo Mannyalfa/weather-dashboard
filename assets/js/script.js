36305e95b540c2736b30a02af3a5c7d166877381
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-06-04 11:04:06 -0400

    adding search criteria in javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,52 @@
 $(document).ready(function () {
+	currentLocation();
     checkLocalStorage();
 });
-
 //API key for ALL weather data
 var APIKey = "4f000a96c8mshf5291b7569fb189p17bea2jsn9e694263a6a9";
 var q = "";
 var now = moment();
-var currentDate = (moment().format("dddd MMMM Do h:mm a"));
+var currentDate = now.format('dddd MMMM Do h:mm a');
 $("#currentDay").text(currentDate);
-
-
 
 //Setting the click function at ID search button
 $("#search-button").on("click", function (event) {
-  
+    // Invalid City Name Entered
+	event.preventDefault();
+    q = $("#city-input").val();
+    if (q === '') {
+        return alert('Please Enter A Valid City Name ! ');
+    }
+    getWeather(q);
 
-});
-
+    saveToLocalStorage(q);
+}); 
 // Function to create Button for searched city 
+function createRecentSearchBtn(q) {
+    var newLi = $("<li>")
+    var newBtn = $('<button>');
+    // prevent duplicate onclick
+    newBtn.attr('id', 'extraBtn');
+    newBtn.addClass("button is-small recentSearch");
+    newBtn.text(q);
+    newLi.append(newBtn)
+    $("#historyList").prepend(newLi);
+    //set click function 
+    $("#extraBtn").on("click", function () {
+        var newQ = $(this).text();
+        getWeather(newQ);
+    });
+//get weather details 
 
-//Function to get weather details 
-
-//open-weather cdn and code snippets DO NOT DELETE!
-
-
-
-
-//---------------------search weather data------------------------------------
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://community-open-weather-map.p.rapidapi.com/find?q=london&cnt=0&mode=null&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric",
+// search weather data w/my API fix in-process
+	function getWeather(q) {
+	var query = "https://community-open-weather-map.p.rapidapi.com/find?q=" + q + "&units=imperial%2C%20metric" +APIKey;
 	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "4f000a96c8mshf5291b7569fb189p17bea2jsn9e694263a6a9",
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+	
+
 	}
 };
-//---------------------get current weather------------------------------------
+//get current weather w/my API-Fix this!!
 const settings = {
 	"async": true,
 	"crossDomain": true,
@@ -54,7 +63,7 @@ $.ajax(settings).done(function (response) {
 });
 
 
-//---------------------get 5 day forecast------------------------------------
+//get 5 day forecast w/my API-Fix this!
 
 const settings = {
 	"async": true,
@@ -78,9 +87,42 @@ $.ajax(settings).done(function (response) {
 
 
 
+/*DATA STORAGE
 
+// get data from Local Storage 
+function checkLocalStorage() {
+    var storedData = localStorage.getItem('queries');
+    var dataArray = [];
+    if (!storedData) {
+        console.log("no data stored");
+    } else {
+        storedData.trim();
+        dataArray = storedData.split(',');
+        for (var i = 0; i < dataArray.length; i++) {
+            createRecentSearchBtn(dataArray[i]);
+        }
+    }
+};
+// set data in Local storage
+function saveToLocalStorage(q) {
+    var data = localStorage.getItem('queries');
+    if (data) {
+        console.log(data, q)
 
-
+    } else {
+        data = q;
+        localStorage.setItem('queries', data);
+    }
+    if (data.indexOf(q) === -1) {
+        data = data + ',' + q;
+        localStorage.setItem('queries', data);
+        createRecentSearchBtn(q);
+    }
+}
+//clear search
+$("#clear-history").on("click", function (event) {
+    $("#historyList").empty();
+});*/
 
 
 
