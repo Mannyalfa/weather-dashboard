e5f8db522308903e4e660f253cc797dca20bc646
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-06-04 14:57:09 -0400

    javaScript complete-need to fix css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,51 @@
-(document).ready(function () {
-	currentLocation();
+$(window).on('load', function () {
+    currentLocation();
     checkLocalStorage();
 });
-//API key for ALL weather data
-var APIKey = "24e07bc5bd24c4619da6c78dabd0b810"
+// API Key 
+var APIKey = "24e07bc5bd24c4619da6c78dabd0b810";
 var q = "";
 var now = moment();
-var currentDate = now.format('dddd MMMM Do h:mm a');
+//Date and time formate for header
+var currentDate = now.format('MMMM Do YYYY : h:mm a');
 $("#currentDay").text(currentDate);
 
 //Setting the click function at ID search button
 $("#search-button").on("click", function (event) {
     // Invalid City Name Entered
-	event.preventDefault();
+    event.preventDefault();
+
     q = $("#city-input").val();
     if (q === '') {
-        return alert('Please Enter A Valid City Name ! ');
+        return alert('Please Enter Valid City Name ! ');
     }
     getWeather(q);
 
     saveToLocalStorage(q);
-}); 
+});
 // Button for searched city  
 function createRecentSearchBtn(q) {
     var newLi = $("<li>")
     var newBtn = $('<button>');
-    // prevent duplicate onclick
+    //Adding Extra ID for Button to stop Creating Duplicate Button on Click
     newBtn.attr('id', 'extraBtn');
     newBtn.addClass("button is-small recentSearch");
     newBtn.text(q);
     newLi.append(newBtn)
     $("#historyList").prepend(newLi);
-    //set click function 
+    //setting click function to prevent duplicate button
     $("#extraBtn").on("click", function () {
         var newQ = $(this).text();
         getWeather(newQ);
     });
-//get weather details change to "q"
+}
+//convert  temp
+function convertToC(fahrenheit) {
+    var fTempVal = fahrenheit;
+    var cTempVal = (fTempVal - 32) * (5 / 9);
+    var celsius = Math.round(cTempVal * 10) / 10;
+    return celsius;
+  }
 
 //Function to get weather details 
 function getWeather(q) {
@@ -46,17 +55,17 @@ function getWeather(q) {
         url: queryURL,
         method: "GET",
         error: (err => { //If API through error then alert 
-            alert("City not found:Please try again.")
+           alert("Your city was not found. Check your spelling or enter a city code")
             return;
-        })
-
+          })
+          
     }).then(function (response) {
         console.log(response)
         //to avoid repeating city information on button click 
         $(".cityList").empty()
         $("#days").empty()
         var celsius = convertToC(response.main.temp);
-        var cityMain1 = $("<div col-12>").append($("<p><h2>" + response.name + ' (' + currentDate + ')' + "</h2><p>"));
+        var cityMain1 = $("<div col-8>").append($("<p><h2>" + response.name + ' (' + currentDate + ')' + "</h2><p>"));
         var image = $('<img class="imgsize">').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');        
         var degreeMain = $('<p>').text('Temperature : ' + response.main.temp + ' °F (' + celsius + '°C)');
         var humidityMain = $('<p>').text('Humidity : ' + response.main.humidity + '%');
@@ -72,10 +81,55 @@ function getWeather(q) {
         $('#cityList').append(cityMain1);
     });
 }
+//function for UV Index
+function displayUVindex(uv) {
+    $.ajax({ // gets the UV index info
+        url: "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + uv,
+        method: "GET"
+    }).then(function (response) {
+        var UVIndex = $("<p><span>");
+        UVIndex.attr("class", "badge badge-danger");
+        UVIndex.text(response.value);
+        $("#cityList").append('UV-Index : ').append(UVIndex);       
+    });
+}
+//function to Display 5 Day forecast
+function displayForecast(c) {
+    $.ajax({ // gets the 5 day forecast API
+        url: "https://api.openweathermap.org/data/2.5/forecast?id=" + c + "&units=imperial&APPID=" + APIKey,
+        method: "GET",
+    }).then(function (response) {
+        //  Parse response to display forecast for next 5 days underneath current conditions
+        var arrayList = response.list;
+        for (var i = 0; i < arrayList.length; i++) {
+            if (arrayList[i].dt_txt.split(' ')[1] === '12:00:00') {
+                console.log(arrayList[i]);
+                var celsius = convertToC(arrayList[i].main.temp);//converting F to Celsius 
+                var cityMain = $('<div>');
+                cityMain.addClass('col forecast bg-primary text-white ml-3 mb-3 rounded>');
+                var date5 = $("<h5>").text(response.list[i].dt_txt.split("MMMM-dd-yyyy")[0]);
+                var image = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + arrayList[i].weather[0].icon + '.png');
+                var degreeMain = $('<p>').text('Temp : ' + arrayList[i].main.temp + ' °F ('+ celsius + '°C)');               
+                var humidityMain = $('<p>').text('Humidity : ' + arrayList[i].main.humidity + '%');
+                var windMain = $('<p>').text('Wind Speed : ' + arrayList[i].wind.speed + 'MPH');                
+                cityMain.append(date5).append(image).append(degreeMain).append(humidityMain).append(windMain);
+                $('#days').append(cityMain);
+            }
+        }
+    });
+};
+// display current location
+function currentLocation() {
+    $.ajax({
+        url: "https://freegeoip.app/json/",
+        method: "GET",
+    }).then(function (response) {
+        q = response.city || 'exton';
+        console.log(q);
+        getWeather(q);
+    });
+};
 
-    
-
-/*DATA STORAGE
 // get data from Local Storage 
 function checkLocalStorage() {
     var storedData = localStorage.getItem('queries');
@@ -95,6 +149,7 @@ function saveToLocalStorage(q) {
     var data = localStorage.getItem('queries');
     if (data) {
         console.log(data, q)
+
     } else {
         data = q;
         localStorage.setItem('queries', data);
@@ -108,8 +163,7 @@ function saveToLocalStorage(q) {
 //clear search
 $("#clear-history").on("click", function (event) {
     $("#historyList").empty();
-});*/
-
+});
 
 
 
